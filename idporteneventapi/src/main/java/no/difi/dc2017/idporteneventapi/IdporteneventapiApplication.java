a2fa84f641f568413d17923dc9bb935b9fa6beaa
Andreas <andrnors@users.noreply.github.com>
2017-06-13 14:17:25 +0200

    initial project commit from spring initializr

---
+++ idporteneventapi/src/main/java/no/difi/dc2017/idporteneventapi/IdporteneventapiApplication.java
@@ -0,0 +1,12 @@
+package no.difi.dc2017.idporteneventapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdporteneventapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdporteneventapiApplication.class, args);
+	}
+}
---
+++ idporteneventapi/src/test/java/no/difi/dc2017/idporteneventapi/IdporteneventapiApplicationTests.java
@@ -0,0 +1,16 @@
+package no.difi.dc2017.idporteneventapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdporteneventapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
